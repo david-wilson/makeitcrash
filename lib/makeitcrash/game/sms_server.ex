24b3f8bf426261db25f1a003238da01969066568
David Wilson <david@davidwilson.me>
2017-07-18 19:43:49 -0600

    Add render game function.

--- lib/makeitcrash/game/sms_server.ex
+++ lib/makeitcrash/game/sms_server.ex
@@ -23,19 +23,23 @@ defmodule Makeitcrash.SmsServer do
         case Map.fetch(state, from) do
             {:ok, player_pid} ->
                 GameServer.guess_letter(player_pid, body)
-                |> inspect
+                |> render_game 
                 |> send_message(from)
                 {:noreply, state}
             :error -> 
                 IO.puts "New game!"
                 {:ok, player_pid} = GameServer.start_link("union")
                 GameServer.guess_letter(player_pid, body)
-                |> inspect
+                |> render_game 
                 |> send_message(from)
                 {:noreply, Map.merge(state, %{from => player_pid})}
         end
     end
 
+    defp render_game({progress, guessed_list, guessed, total, state} = game_tuple) do
+        rendered_list = inspect(guessed_list)
+        "#{progress}, #{guessed}/#{total} guesses, #{rendered_list}"
+    end
 
     defp send_message(body, number) do
         message = %ExTwilio.Message{from: Application.get_env(:ex_twilio, :from_number),
