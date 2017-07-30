c944d238cc7f9f7383760ed358d433f0398d3012
David Wilson <david@davidwilson.me>
2017-07-30 08:15:52 -0600

    rename SMS controller file.

---
+++ lib/makeitcrash/web/controllers/sms_controller.ex
@@ -0,0 +1,9 @@
+defmodule Makeitcrash.Web.SmsController do
+    use Makeitcrash.Web, :controller
+
+    def webhook(conn, _params) do
+        %{"From" => from, "Body" => body} = conn.body_params 
+        Makeitcrash.SmsHandler.handle_message(from, body)
+        send_resp(conn, 200, "")
+    end
+end
