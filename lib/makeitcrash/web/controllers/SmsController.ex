cd91091f7175c26f11579621f917309c94a90bac
David Wilson <david@davidwilson.me>
2017-07-30 08:28:49 -0600

    Add words, add delete.

--- lib/makeitcrash/web/controllers/SmsController.ex
+++
@@ -1,9 +0,0 @@
-defmodule Makeitcrash.Web.SmsController do
-    use Makeitcrash.Web, :controller
-
-    def webhook(conn, _params) do
-        %{"From" => from, "Body" => body} = conn.body_params 
-        Makeitcrash.SmsHandler.handle_message(from, body)
-        send_resp(conn, 200, "")
-    end
-end
